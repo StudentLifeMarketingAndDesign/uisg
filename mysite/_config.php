c571af84f74000ec66f527214cbddb379efce11e
Dustin Quam <quamsta@gmail.com>
2014-02-24 15:34:51 -0600

    changed config for dev

--- mysite/_config.php
+++ mysite/_config.php
@@ -4,7 +4,7 @@ global $project;
 $project = 'mysite';
 
 global $database;
-$database = 'uisg';
+$database = 'uisg-kit';
  
 // Use _ss_environment.php file for configuration
 require_once("conf/ConfigureFromEnv.php");
